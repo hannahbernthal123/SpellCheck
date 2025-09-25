4ca76e4e1a05f66a1b64573b6f11ae30b1e0573d
hannahbernthal <hannah.bernthal@menloschool.org>
2025-09-25 15:30:54 -0700

    first drafts...fails.

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -7,9 +7,8 @@
  * Completed by: Hannah Bernthal
  * */
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class SpellCheck {
 
@@ -25,30 +24,53 @@ public class SpellCheck {
         ArrayList<String> textClone = new ArrayList<>();
         String[] perm = new String[text.length];
 
-        for (int i = 0; i < text.length - 1; i++) {
-            for (int j = 0; j < dictionary.length - 1; j++) {
-                if (dictionary[j] == text[i]) {
-                    textClone.remove(i);
-                }
+        // Collect matches (preserve order, remove duplicates)
+        LinkedHashSet<String> matches = new LinkedHashSet<>();
+
+        for (String w : text) {
+            if (binarySearch(dictionary, w)) {
+                matches.add(w);
             }
         }
 
-        int i = 0;
-        while (i < textClone.size() - 1) {
-            String word = textClone.get(i);
-            if (word.equals(textClone.get(i + 1)))
-                textClone.remove(i + 1);
-            else
-                i++;
-        }
-
-        for (int k = 0; k < textClone.size(); k++) {
-            perm[k] = textClone.get(k);
-        }
-
-        return perm;
+        return matches.toArray(new String[0]);
     }
 
+    private boolean binarySearch(String[] sorted, String target) {
+            int low = 0, hi = sorted.length - 1;
+            while (low <= hi) {
+                int mid = low + (hi - low) / 2;
+                int cmp = sorted[mid].compareTo(target);
+                if (cmp == 0) return true;
+                if (cmp < 0)  low = mid + 1;
+                else          hi = mid - 1;
+            }
+            return false;
 
+    }
+
+//
+//        for (int i = 0; i < text.length - 1; i++) {
+//            for (int j = 0; j < dictionary.length - 1; j++) {
+//                if (dictionary[j] == text[i]) {
+//                    textClone.remove(i);
+//                }
+//            }
+//        }
+//
+//        int i = 0;
+//        while (i < textClone.size() - 1) {
+//            String word = textClone.get(i);
+//            if (word.equals(textClone.get(i + 1)))
+//                textClone.remove(i + 1);
+//            else
+//                i++;
+//        }
+//
+//        for (int k = 0; k < textClone.size(); k++) {
+//            perm[k] = textClone.get(k);
+//        }
+//
+//        return perm;
 
 }
